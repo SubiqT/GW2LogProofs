c89126598868cd28566c172ced9314d3d1021284
Matthew Read <mjr34d@gmail.com>
2024-05-28 10:32:36 +0200

    move ownership of players vector to log_proofs

--- src/shared.cpp
+++ src/shared.cpp
@@ -1,9 +1,6 @@
-#include <chrono>
-#include <mutex>
 #include <filesystem>
 
 #include "nexus/Nexus.h"
-#include "shared.h"
 
 const char* ADDON_NAME = "Log Proofs";
 
@@ -13,6 +10,3 @@ std::filesystem::path SettingsPath;
 AddonAPI* APIDefs = nullptr;
 NexusLinkData* NexusLink = nullptr;
 HMODULE hSelf = nullptr;
-
-std::vector<Player> players;
-std::string selfName;
--- src/shared.h
+++ src/shared.h
@@ -1,14 +1,9 @@
 #ifndef SHARED_H
 #define SHARED_H
 
-#include <vector>
-#include <map>
-#include <string>
 #include <filesystem>
 
 #include "nexus/Nexus.h"
-
-#include "arcdps/arcdps.h"
 
 extern const char* ADDON_NAME;
 
@@ -19,13 +14,5 @@ extern AddonAPI* APIDefs;
 extern NexusLinkData* NexusLink;
 extern HMODULE hSelf;
 
-struct Player {
-    std::string account;
-    std::vector<std::string> characters;
-    std::vector<std::string> groups;
-    std::map<std::string, std::map<std::string, int>> kp;
-};
-extern std::vector<Player> players;
-extern std::string selfName;
 
 #endif
